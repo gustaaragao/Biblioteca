cd05baa046809217d621d599d37fff488ca8fdd4
gustaaragao <gustavohenriquearagaosilva@gmail.com>
2025-08-04 20:22:32 -0300

    atualizando template

--- Codigo/Tree/bit.cpp
+++ Codigo/Tree/bit.cpp
@@ -39,15 +39,3 @@ struct fenw {
         return i - 1;
     }
 };
-
-
-int main() {
-    
-    int arr[5] = {1, 2, 3, 4, 5};
-
-    for (int i = 0; i < 5; i++) {
-        
-    }
-
-    return 0;
-}
